854d8909ee8f50a1e564bcf6c53c6c3f659ea86c
vaidude <vaivaideesh33@gmail.com>
2024-11-21 05:35:35 +0530

    vercelconfig

--- PAWSTAILS/settings.py
+++ PAWSTAILS/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-(zp=@3lki2nrbht-_h$#pqjv1zosv@p8nhbbtihvf=-6f$5n+l
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -122,7 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assests')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
