248893465f27542c76dfc706faa2198fb8604a59
vaidude <vaivaideesh33@gmail.com>
2024-11-21 10:07:56 +0530

    updates

--- PAWSTAILS/settings.py
+++ PAWSTAILS/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
+MEDIA_URL='media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
