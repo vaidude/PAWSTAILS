c4f72da5b131907a425dccdeaec2b919749e35c7
vaidude <vaivaideesh33@gmail.com>
2024-11-21 11:45:04 +0530

    wsgiconf

--- PAWSTAILS/wsgi.py
+++ PAWSTAILS/wsgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PAWSTAILS.settings')
 
 application = get_wsgi_application()
-
